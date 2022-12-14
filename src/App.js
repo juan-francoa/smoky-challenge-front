707538cf1ea8c713d91def9422cdfb6f3dc9c217
Meli Uribe <lissauribe21@gmail.com>
2022-12-14 10:59:07 -0500

    HTML version

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+/* import "bootstrap/dist/css/bootstrap.min.css" */
+
+function App() {
+  return (
+    <Navbar />
+
+  )
+}
+
+
+{/* <div>
+      <header className='w-100 vh-100'>
+            <video className='videoHome' src="./img/video/headerBackground.mp4" autoPlay muted loop></video>
+        </header>
+    </div> */}
+
+
+export default App;
