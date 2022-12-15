88645b01fe5a2122c060aeaaeb68d00ee2a09d7d
juan-francoa <esteban_rlt159@hotmail.com>
2022-12-15 11:35:05 -0500

    primeras pags

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React from 'react'
-import Navbar from './components/Navbar';
-/* import "bootstrap/dist/css/bootstrap.min.css" */
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Layout from './layouts/Layout';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+import Shopping from './pages/Shopping';
+import Vapers from './pages/Vapers';
 
 function App() {
   return (
-    <Navbar />
-
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/*" element={<Home />} />
+          <Route path="/signin" element={<SignIn/>} />
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/shopping" element={<Shopping/>} />
+          <Route path="/vapers" element={<Vapers/>} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   )
 }
 
 
-{/* <div>
-      <header className='w-100 vh-100'>
-            <video className='videoHome' src="./img/video/headerBackground.mp4" autoPlay muted loop></video>
-        </header>
-    </div> */}
 
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <header className='w-100 vh-100'>
+        <p className="text-video">
+            Smoky offers the widest variety and longest lasting vaping products on the market. Check out our complete
+            lineup of vape devices for every type of vaping experience.
+        </p>
+        <video className='videoHome' src="img/video/headerBackground.mp4" autoPlay muted loop></video>
+    </header>
+
+    <main>
+        <h2 className="text-white categories-text">Visit our awesome experience</h2>
+        <section className="images-section d-flex flex-wrap">
+            <div className="img-category-1">Equipments</div>
+            <div className="img-category-2">Liquids</div>
+            <div className="img-category-3">Disposables</div>
+            <div className="img-category-4">Flavors</div>
+        </section>
+        <div className="aboutus-container">
+            <div className="text-container d-flex ">
+                <h2 className="text-white">What do we do?</h2>
+                <p className="text-light">
+                    At Smooky, we strive to develop and bring to the market patented electronic cigarette devices of
+                    the
+                    highest quality, best flavors, and under numerous brands that have been distributed all over the
+                    globe.
+
+                    Each of these brands have been highly successful because of its extensive market research and
+                    development before a product ever sees the market. Smoky continues to develop the best flavored,
+                    best
+                    devices, and most popular brands, as we focus on what our customers want, and what we know how
+                    to do
+                    best, listen to them and create the best products in the market.
+
+                    So as you check our brands make sure that you are buying a genuine Smoky product by making sure
+                    the
+                    Smoky trademark, QRJOY(R) is on the product along with out branding name. We look forward to
+                    bringing
+                    you excellent brands, and excellent products as we continue to take the vaping world by storm.
+                </p>
+            </div>
+            <div className="img-container">
+                <img className="main-img" src="img/main-home-img.jpg" alt="man vaping" />
+            </div>
+        </div>
+    </main>
+    </div>
+  )
+}
