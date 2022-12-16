25a3b89b0f6f0835ee7ab0b089f1fcb9920bac1d
Your Name01 <rocioduarte197@gmail.com>
2022-12-16 18:12:58 -0300

    signIn-signUp

--- src/redux/reducers/mainReducer.js
+++ src/redux/reducers/mainReducer.js
@@ -1,10 +1,13 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import vapersReducer from "./vapersReducer";
 import eliquidsReducer from "./eliquidsReducer";
+import userReducer from "./userReducer";
 
 const mainReducer = combineReducers({
     vapersReducer,
     eliquidsReducer,
+    userReducer
+
 })
 
 export default mainReducer
