1f6d1e2c2a6562fcc3e3f686c31411d70d95b7e2
Your Name01 <rocioduarte197@gmail.com>
2022-12-22 19:48:57 -0300

    myprofile

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,30 +1,86 @@
 import React from 'react'
 import { useDispatch, useSelector } from "react-redux";
+import userActions from '../redux/actions/userAction';
+import Swal from 'sweetalert2';
 import "../profile.css"
 
 export default function MyProfile() {
-    let { photo, name, email} = useSelector((store) => store.userReducer);
-    console.log( email)
+    const dispatch = useDispatch()
+    const { updateMyProfile } = userActions;
+    let { myUser, token } = useSelector(store => store.userReducer);
+    console.log(myUser);
+    let id
+
+    async function updateUser() {
+        try {
+            const { value: formValues } = await Swal.fire({
+                title: 'Update User',
+                showCancelButton: true,
+                confirmButtonText: 'Update',
+                html:
+                    '<input placeHolder="Photo Url"id="photo" class="swal2-input">' +
+                    '<input placeHolder="Name" id="name" class="swal2-input">' +
+                    '<input placeHolder="Age"id="age" class="swal2-input">' +
+                    '<input placeHolder="Email"id="email" class="swal2-input">',
+                focusConfirm: false,
+                preConfirm: () => {
+                    let photo = document.getElementById('photo').value
+                    let name = document.getElementById('name').value
+                    let age = document.getElementById('age').value
+                    let email = document.getElementById('email').value
+
+                    let data = {
+                        id: myUser.id,
+                        user: {
+                        }
+                    }
+                    if (photo !== '') {
+                        data.user.photo = photo
+                    }
+                    if (name !== '') {
+                        data.user.name = name
+                    }
+                    if (age !== '') {
+                        data.user.age = age
+                    }
+                    if (email !== '') {
+                        data.user.email = email
+                    }
+                    dispatch(updateMyProfile(data))
+                }
+            })
+
+            if (formValues) {
+                Swal.fire(JSON.stringify(formValues))
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <main className='containerprofile'>
-                <div class="profile-card text-white ">
-                    <div class="profile-img">
-                        <img class="profile-im" src={photo} alt=""  />
-                        </div>
-                    <div class="container-info">
-                        <div class="name-container">
-                            <img class="edit-img" src="https://cdn-icons-png.flaticon.com/512/1160/1160515.png" />
-                                <h1>{name}</h1>
-                        </div>
-                        <div class="name-container">
-                            <img class="edit-img" src="https://cdn-icons-png.flaticon.com/512/1160/1160515.png" />
-                                <p>Email: {email}</p>
-                        </div>
-                        <div class="name-container">
-                            <img class="edit-img" src="https://cdn-icons-png.flaticon.com/512/1160/1160515.png" />
-                                <p>Contraseña: hola123</p>
-                        </div>
-                   
+            <div class="profile-card text-white ">
+                <div class="profile-img">
+                    <img class="profile-im" src={myUser.photo} alt="" />
+                </div>
+                <div class="container-info">
+                    <div class="name-container">
+                        <img class="edit-img" src="https://cdn-icons-png.flaticon.com/512/1160/1160515.png" />
+                        <h1>{myUser.name}</h1>
+                    </div>
+                    <div class="name-container d-flex align-items-center gap-3">
+                        <img class="edit-img" src="https://cdn-icons-png.flaticon.com/512/1160/1160515.png" />
+                        <h1>Age:{myUser.age}</h1>
+                    </div>
+                    <div class="name-container">
+                        <img class="edit-img" src="https://cdn-icons-png.flaticon.com/512/1160/1160515.png" />
+                        <p>Email: {myUser.email}</p>
+                    </div>
+                    <div class="name-container">
+                        <button className='bottom-cards' onClick={updateUser}>Update Profile</button>
+                    </div>
+
                 </div>
             </div>
         </main>
